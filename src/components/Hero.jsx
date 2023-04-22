0c191af813659d0d3ac9372635f8dd8ca8a6136a
Nanak <nanakbandyopadhyay@hotmail.com>
2023-04-22 21:29:48 +0530

    add 3d computer, loader and make 3d model mobile responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,11 +17,29 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Nanak</span>
           </h1>
           <span className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I develop full-stack applications for the web & web3 
+            I develop full-stack applications for the web & web3
           </span>
         </div>
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas />
+
+      <div className="absolute w-full xs:bottom-10 bottom-32 flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
